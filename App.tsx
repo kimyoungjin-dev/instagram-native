c6d87405283dcf505dc564b5dd29a63004d88d4f
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-17 20:02:28 +0900

    Write startAsync, onError and onFinish as AppLoading props.
    If it is loading, execute AppLoading => When AppLoading is finished, the Loading state is finished.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [loading, setLoading] = useState(true);
+  const preload = async () => {};
+  const onFinish = () => setLoading(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
+}
