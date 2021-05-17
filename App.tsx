2652fd1264f2f9e73b6e27338bef2963620c5c6a
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-17 20:27:56 +0900

    preload: Create Font.loadAsync(font) by using map function for each font of Ionicons.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { View, Text } from "react-native";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const preload = async () => {};
+  const preload = async () => {
+    const fontToLoad = [Ionicons.font];
+    const fontPromiss = fontToLoad.map((font: any) => Font.loadAsync(font));
+    Promise.all(fontPromiss);
+  };
   const onFinish = () => setLoading(false);
 
   if (loading) {
@@ -15,4 +22,10 @@ export default function App() {
       />
     );
   }
+
+  return (
+    <View>
+      <Text>is ready</Text>
+    </View>
+  );
 }
