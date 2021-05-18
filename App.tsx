67e2ab701b3c7952a6eecf5de6f3a17a479e6827
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-18 21:10:24 +0900

    Write apploading props
    Using Promise.all, download fonts and images in advance.
    
    Wrap LoggedOutNav (stack navigation) with NavigationContainer.

--- App.tsx
+++ App.tsx
@@ -2,16 +2,35 @@ import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { View, Text } from "react-native";
+import { Asset } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import LoggedOutNav from "./Navigator/LoggedOutNav";
+import { Image } from "react-native";
+
+const getImages = (images: any) =>
+  images.map((image: any) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const preload = async () => {
-    const fontToLoad = [Ionicons.font];
-    const fontPromiss = fontToLoad.map((font: any) => Font.loadAsync(font));
-    Promise.all(fontPromiss);
-  };
   const onFinish = () => setLoading(false);
+
+  const preload = async () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromise = fontsToLoad.map((font) => Font.loadAsync(font));
+
+    const imagePromise = getImages([
+      require("./assets/instagram_logo.png"),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
+    ]);
+
+    await Promise.all([...fontPromise, ...imagePromise]);
+  };
 
   if (loading) {
     return (
@@ -24,8 +43,8 @@ export default function App() {
   }
 
   return (
-    <View>
-      <Text>is ready</Text>
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
