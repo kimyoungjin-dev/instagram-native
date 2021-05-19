9c998137eee756eb64abcd30f634d166380413bc
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-20 03:31:53 +0900

    Construct light and dark themes and specify the type.

---
+++ styles/styled.d.ts
@@ -0,0 +1,11 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    bgColor: string;
+    fontColor: string;
+    loginBtnColor: string;
+    borderColor: string;
+    silverColor: string;
+  }
+}
---
+++ styles/styles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle, DefaultTheme } from "styled-components";
+import { reset } from "styled-reset";
+
+export const light: DefaultTheme = {
+  bgColor: "#FAFAFA",
+  fontColor: "rgb(38,38,38)",
+  loginBtnColor: "#0095f6",
+  borderColor: "rgb(219,219,219)",
+  silverColor: "silver",
+};
+
+export const dark: DefaultTheme = {
+  bgColor: "rgb(38,38,38)",
+  fontColor: "#FAFAFA",
+  loginBtnColor: "#0095f6",
+  borderColor: "rgb(219,219,219)",
+  silverColor: "silver",
+};
