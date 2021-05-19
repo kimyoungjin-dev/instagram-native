4e8e9ae6fb38c31416ac540bdd63fd337ea0c801
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-20 03:34:08 +0900

    Wrap App.tsx with ThemeManager.

--- App.tsx
+++ App.tsx
@@ -6,24 +6,24 @@ import { Asset } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
 import LoggedOutNav from "./Navigator/LoggedOutNav";
 import { Image } from "react-native";
+import ThemeManager from "./styles/ChangeMode";
 
-const getImages = (images: any) =>
-  images.map((image: any) => {
+function getImages(images: any) {
+  return images.map((image: any) => {
     if (typeof image === "string") {
       return Image.prefetch(image);
     } else {
       return Asset.fromModule(image).downloadAsync();
     }
   });
+}
 
 export default function App() {
   const [loading, setLoading] = useState(true);
   const onFinish = () => setLoading(false);
-
   const preload = async () => {
     const fontsToLoad = [Ionicons.font];
     const fontPromise = fontsToLoad.map((font) => Font.loadAsync(font));
-
     const imagePromise = getImages([
       require("./assets/instagram_logo.png"),
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
@@ -43,8 +43,10 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <LoggedOutNav />
-    </NavigationContainer>
+    <ThemeManager>
+      <NavigationContainer>
+        <LoggedOutNav />
+      </NavigationContainer>
+    </ThemeManager>
   );
 }
