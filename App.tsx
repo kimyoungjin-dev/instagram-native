b46bc4a5f34f3a45c45f2b3d4570ffe35e80fa18
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-21 01:21:47 +0900

    Error correction: Icon not available => Icon available

--- App.tsx
+++ App.tsx
@@ -1,55 +1,57 @@
 import AppLoading from "expo-app-loading";
-import React, { useState } from "react";
-import * as Font from "expo-font";
-import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
+import React, { useState } from "react";
+import { Image } from "react-native";
+import * as Font from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import LoggedOutNav from "./Navigator/LoggedOutNav";
-import { Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import ThemeManager from "./styles/ChangeMode";
 import { AppearanceProvider } from "react-native-appearance";
 
-function getImages(images: any) {
-  return images.map((image: any) => {
+const getImages = (images: any) =>
+  images.map((image: any) => {
     if (typeof image === "string") {
       return Image.prefetch(image);
     } else {
       return Asset.fromModule(image).downloadAsync();
     }
   });
-}
+
+const getFonts = (fonts: any) =>
+  fonts.map((font: any) => [Font.loadAsync(font)]);
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const onFinish = () => setLoading(false);
-  const preload = async () => {
-    const fontsToLoad = [Ionicons.font];
-    const fontPromise = fontsToLoad.map((font) => Font.loadAsync(font));
-    const imagePromise = getImages([
-      require("./assets/instagram_logo.png"),
+
+  const loadAssets = async () => {
+    const images = getImages([
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
+      require("./assets/instagram_logo.png"),
     ]);
-
-    await Promise.all([...fontPromise, ...imagePromise]);
+    const fonts = getFonts([Ionicons.font]);
+    await Promise.all([...images, ...fonts]);
   };
-
-  if (loading) {
-    return (
-      <AppLoading
-        startAsync={preload}
-        onError={console.warn}
-        onFinish={onFinish}
-      />
-    );
-  }
-
+  const onFinish = () => setLoading(false);
   return (
-    <AppearanceProvider>
-      <ThemeManager>
-        <NavigationContainer>
-          <LoggedOutNav />
-        </NavigationContainer>
-      </ThemeManager>
-    </AppearanceProvider>
+    <>
+      {loading ? (
+        <>
+          <AppearanceProvider>
+            <ThemeManager>
+              <NavigationContainer>
+                <LoggedOutNav />
+              </NavigationContainer>
+            </ThemeManager>
+          </AppearanceProvider>
+        </>
+      ) : (
+        <AppLoading
+          startAsync={loadAssets}
+          onError={console.warn}
+          onFinish={onFinish}
+        />
+      )}
+    </>
   );
 }
