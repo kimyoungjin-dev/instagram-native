fec543f2c7b2f92511617e84107508dbbf13d80c
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-21 01:24:49 +0900

    Update Login.tsx
    Add Subtitle: Create a subtitle and show the name of the current router.
    
    Compose Form in the same way as CreateAccount.tsx.
    
    Use KeyboardAvoidingView
    
    Add SwitchBox

--- Pages/Login.tsx
+++ Pages/Login.tsx
@@ -1,14 +1,61 @@
-import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import React, { useRef } from "react";
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TextInputBase,
+  View,
+} from "react-native";
+import AuthButton from "../Components/Auth/AuthButton";
+import AuthLayOut from "../Components/Auth/AuthLayOut";
+import Subtitle from "../Components/Auth/Subtitle";
+import { Input } from "../Components/Auth/TextInputStyles";
+import SwitchBox from "../Components/SwitchBox";
+import { useTheme } from "../styles/ChangeMode";
 import { AuthProps } from "../utils/AuthParamList";
 
 export default function Login({ navigation }: AuthProps<"Login">) {
+  const theme = useTheme();
+  const passwordRef = useRef(null);
+
+  const onNext = (name: React.MutableRefObject<any>) => {
+    name?.current?.focus();
+  };
+
+  const LastInput = () => {
+    Alert.alert("확인");
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
-        <Text>go to CreateAccount</Text>
-      </TouchableOpacity>
-    </View>
+    <AuthLayOut>
+      <SwitchBox />
+      <Subtitle text="Login" />
+      <KeyboardAvoidingView
+        style={{
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        behavior="padding"
+        keyboardVerticalOffset={Platform.OS === "ios" ? 40 : 0}
+      >
+        <Input
+          placeholder="First Name"
+          returnKeyType="next"
+          placeholderTextColor={theme.mode === "dark" ? "black" : "white"}
+          onSubmitEditing={() => onNext(passwordRef)}
+        />
+
+        <Input
+          ref={passwordRef}
+          placeholder="First Name"
+          returnKeyType="next"
+          placeholderTextColor={theme.mode === "dark" ? "black" : "white"}
+          onSubmitEditing={() => LastInput()}
+        />
+        <AuthButton disabled={true} onPress={() => null} text="Login" />
+      </KeyboardAvoidingView>
+    </AuthLayOut>
   );
 }
