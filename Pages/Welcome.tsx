1508ae78c5d0668661d3f6f3aa11c0f3b211eb27
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-20 15:23:06 +0900

    Update Welcome.tsx
    Define the navigation function
    
    css design
    
    When activated by adding disable to a button, opacity: 1 When deactivated: Less than 1.

--- Pages/Welcome.tsx
+++ Pages/Welcome.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components/native";
 import SwitchBox from "../Components/SwitchBox";
+import { AuthProps } from "../utils/AuthParamList";
 
 const Container = styled.View`
   flex: 1;
@@ -9,28 +10,61 @@ const Container = styled.View`
   background-color: ${(props) => props.theme.bgColor};
 `;
 
+const LogoBox = styled.View`
+  margin-bottom: 50px;
+`;
+
 const Logo = styled.Text`
   font-size: 50px;
   color: ${(props) => props.theme.fontColor};
   font-weight: bold;
 `;
 
-const CreateAccount = styled.View`
+const CreateAccount = styled.TouchableOpacity`
   background-color: ${(props) => props.theme.loginBtnColor};
+  color: ${(props) => props.theme.fontColor};
+  text-align: center;
+  padding: 10px 9px;
+  border-radius: 5px;
+  width: 90%;
+  opacity: ${(props) => (props.disabled ? "0.4" : "1")};
 `;
 
 const CreateAccountText = styled.Text`
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  font-size: 15px;
+`;
+
+const LoginBtn = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+
+const LoginText = styled.Text`
+  text-align: center;
+  font-size: 18px;
   color: ${(props) => props.theme.fontColor};
 `;
 
-export default function Welcome() {
+export default function Welcome({ navigation }: AuthProps<"Welcome">) {
+  const goToCreateAccount = () => navigation.navigate("CreateAccount");
+  const goToLogin = () => navigation.navigate("Login");
+
   return (
     <Container>
       <SwitchBox />
-      <Logo>Instagram</Logo>
-      <CreateAccount>
+      <LogoBox>
+        <Logo>Instagram</Logo>
+      </LogoBox>
+
+      <CreateAccount onPress={() => goToCreateAccount()} disabled={false}>
         <CreateAccountText>CreateAccount</CreateAccountText>
       </CreateAccount>
+
+      <LoginBtn onPress={() => goToLogin()}>
+        <LoginText>Log in</LoginText>
+      </LoginBtn>
     </Container>
   );
 }
