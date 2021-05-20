d9581da6868950ecbf53f543cee0fead4439783e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-20 22:18:23 +0900

    Change css property

--- styles/styles.ts
+++ styles/styles.ts
@@ -1,16 +1,16 @@
 import { DefaultTheme } from "styled-components";
 
 export const light: DefaultTheme = {
-  bgColor: "white",
-  fontColor: "black",
+  bgColor: "#FAFAFA",
+  fontColor: "rgb(38,38,38)",
   loginBtnColor: "blue",
   borderColor: "rgb(219,219,219)",
   silverColor: "silver",
 };
 
 export const dark: DefaultTheme = {
-  bgColor: "black",
-  fontColor: "white",
+  bgColor: "rgb(38,38,38)",
+  fontColor: "#FAFAFA",
   loginBtnColor: "blue",
   borderColor: "rgb(219,219,219)",
   silverColor: "silver",
