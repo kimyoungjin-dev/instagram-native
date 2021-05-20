e1fd20fa48d4af92acdceeedbc03cfb73be2d190
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-20 22:17:08 +0900

    Recycle submit button

---
+++ Components/Auth/AuthButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { GestureResponderEvent } from "react-native";
+import styled from "styled-components/native";
+
+interface Props {
+  onPress: (event: GestureResponderEvent) => void;
+  disabled: boolean;
+  text: string;
+}
+
+const Button = styled.TouchableOpacity`
+  background-color: ${(props) => props.theme.loginBtnColor};
+  text-align: center;
+  padding: 10px 9px;
+  border-radius: 5px;
+  width: 90%;
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  font-size: 15px;
+`;
+
+export default function AuthButton({ onPress, disabled, text }: Props) {
+  return (
+    <Button onPress={onPress} disabled={disabled}>
+      <ButtonText>{text}</ButtonText>
+    </Button>
+  );
+}
