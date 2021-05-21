286a04049443e79fc1ba3b1653982e53c9f81532
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-21 21:26:18 +0900

    Create logUserIn function, create tokenVar variable
    The initial value of tokenVar is an empty string.
    
    logUserIn function: A function that stores tokens using multiSet

--- Components/Apollo.tsx
+++ Components/Apollo.tsx
@@ -1,10 +1,18 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
+export const tokenVar = makeVar("");
+
+// multiSet을 이용하여 token을 저장한다.
+export const logUserIn = async (token: string) => {
+  await AsyncStorage.multiSet([["token", JSON.stringify(token)]]);
+  isLoggedInVar(true);
+  tokenVar(token);
+};
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
-
 export default client;
