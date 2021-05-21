b145b8d42bcd7e5e5f9ab941361227f404a8d708
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-22 05:42:02 +0900

    Use setContext to put token in header & It connects authLink and httpLink.
    If you call without putting a value in tokenVar, only the current value is retrieved.

--- Components/Apollo.tsx
+++ Components/Apollo.tsx
@@ -5,6 +5,7 @@ import {
   makeVar,
 } from "@apollo/client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { setContext } from "@apollo/client/link/context";
 
 export const isLoggedInVar = makeVar(false);
 export const tokenVar = makeVar("");
@@ -19,9 +20,20 @@ export const logUserIn = async (token: string) => {
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
 });
+//token을 헤더에 넣기위해서 setContext 사용 (setContext는 자동 import가 안됨으로 import경로를 알고있어야한다.)
+//tokenVar안에 value를 넣지않고 호출만 한다면, 현재 value만을 받아온다.
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: tokenVar(),
+    },
+  };
+});
 
+//authLink와 httpLink를 연결 해준다.
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 export default client;
