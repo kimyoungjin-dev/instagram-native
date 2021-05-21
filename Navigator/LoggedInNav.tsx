01a591b9cb29136310f2523742d233549971cc46
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-21 21:59:54 +0900

    Create tabBarIcon
    tabBarIcon: Add icon option.
    tabBarOptions.showLabel: false Setting: Delete the name of the tab.

--- Navigator/LoggedInNav.tsx
+++ Navigator/LoggedInNav.tsx
@@ -1,13 +1,64 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Feed from "../Pages/Feed";
+import Search from "../Pages/Search";
+import Notification from "../Pages/Notification";
+import Profile from "../Pages/Profile";
+import { useTheme } from "../styles/ChangeMode";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tabs = createBottomTabNavigator();
 
 export default function LoggedInNav() {
+  const theme = useTheme();
+
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Feed" component={Feed} />
+    <Tabs.Navigator
+      tabBarOptions={{
+        activeTintColor: "white",
+        showLabel: false,
+        style: {
+          backgroundColor: theme.mode === "dark" ? "white" : "black",
+          borderTopColor: theme.mode === "dark" ? "white" : "black",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="home" color={color} size={focused ? 22 : 18} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="search" color={color} size={focused ? 22 : 18} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Notification"
+        component={Notification}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="heart" color={color} size={focused ? 22 : 18} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="person" color={color} size={focused ? 22 : 18} />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 }
