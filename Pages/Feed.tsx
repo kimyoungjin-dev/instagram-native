da6a32d804bca8454cac5fbc9124ddf17515d1b3
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-22 06:24:15 +0900

    Create FEED_QUERY
    Create FEED_QUERY
    
    ${COMMENT_FRAGMENT} ${PHOTO_FRAGMENT} : Fragment import

--- Pages/Feed.tsx
+++ Pages/Feed.tsx
@@ -1,7 +1,36 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
+import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../Components/Fragment";
+
+const FEED_QUERY = gql`
+  query seeFeed($page: Int!) {
+    seeFeed(page: $page) {
+      ...PhotoFragment
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      commentNumber
+      comments {
+        ...CommentFragment
+      }
+      createdAt
+      isMine
+      isLiked
+    }
+  }
+  ${COMMENT_FRAGMENT}
+  ${PHOTO_FRAGMENT}
+`;
 
 export default function Feed({ navigation }: any) {
+  const { data } = useQuery(FEED_QUERY);
   return (
     <View
       style={{
@@ -11,9 +40,7 @@ export default function Feed({ navigation }: any) {
         backgroundColor: "black",
       }}
     >
-      <TouchableOpacity onPress={() => navigation.navigate("Photo")}>
-        <Text style={{ color: "white" }}>Photo</Text>
-      </TouchableOpacity>
+      <Text style={{ color: "white" }}>Photo</Text>
     </View>
   );
 }
