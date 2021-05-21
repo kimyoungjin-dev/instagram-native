65cbd2e7872fcbe69da5192f238e2bb3c97a6047
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-21 18:39:59 +0900

    Make LOGINT_MUTATION
    If the login is successful, it changes isLoggedInVar to true.
    
    The props received from createAccount are designated as the defaultvalue of username. (Unlike the front end, you must set a value in TextInput)

--- Pages/Login.tsx
+++ Pages/Login.tsx
@@ -1,20 +1,56 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
 import React, { useEffect, useRef } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { KeyboardAvoidingView, Platform } from "react-native";
+import { isLoggedInVar } from "../Components/Apollo";
 import AuthButton from "../Components/Auth/AuthButton";
 import AuthLayOut from "../Components/Auth/AuthLayOut";
 import Subtitle from "../Components/Auth/Subtitle";
 import { Input } from "../Components/Auth/TextInputStyles";
 import SwitchBox from "../Components/SwitchBox";
 import { useTheme } from "../styles/ChangeMode";
+import { login, loginVariables } from "../__generated__/login";
 
-interface IProps {
-  username: string;
-  password: string;
-}
+const LOGINT_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      ok
+      error
+      token
+    }
+  }
+`;
 
-export default function Login() {
-  const { register, handleSubmit, setValue } = useForm();
+export default function Login({ route: { params } }: any) {
+  const { register, handleSubmit, setValue, watch } = useForm({
+    defaultValues: {
+      username: params?.username,
+      password: params?.password,
+    },
+  });
+  const [loginMutation, { loading }] = useMutation<login, loginVariables>(
+    LOGINT_MUTATION,
+    {
+      onCompleted: (data) => {
+        const {
+          login: { ok },
+        } = data;
+
+        if (ok) return isLoggedInVar(true);
+      },
+    }
+  );
+
+  const onSubmit: SubmitHandler<loginVariables> = (data) => {
+    if (!loading) {
+      loginMutation({
+        variables: {
+          ...data,
+        },
+      });
+    }
+  };
 
   const theme = useTheme();
   const passwordRef = useRef(null);
@@ -23,13 +59,14 @@ export default function Login() {
     value?.current?.focus();
   };
 
-  const onSubmit: SubmitHandler<IProps> = (data) => console.log(data);
-
   useEffect(() => {
     register("username");
     register("password");
   }, [register]);
 
+  //AuthButton  disabled props는 watch에 의해 username password둘다 빈배열이 아니어야 활성화된다.
+
+  watch;
   return (
     <AuthLayOut>
       <SwitchBox />
@@ -44,6 +81,7 @@ export default function Login() {
         keyboardVerticalOffset={Platform.OS === "ios" ? 40 : 0}
       >
         <Input
+          value={watch("username")}
           placeholder="User Name"
           returnKeyType="next"
           autoCapitalize="none"
@@ -55,6 +93,7 @@ export default function Login() {
         />
 
         <Input
+          value={watch("password")}
           ref={passwordRef}
           placeholder="password"
           returnKeyType="done"
@@ -65,10 +104,10 @@ export default function Login() {
           }}
         />
         <AuthButton
-          disabled={true}
+          disabled={!watch("username") || !watch("password")}
           onPress={handleSubmit(onSubmit)}
           text="Login"
-          loading={true}
+          loading={loading}
         />
       </KeyboardAvoidingView>
     </AuthLayOut>
