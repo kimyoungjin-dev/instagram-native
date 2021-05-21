73ffe0e83f1375a2e8c3c0524d40488c9573b9f8
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-22 08:31:56 +0900

    Connect with Photo component
    Give 100% of the width of FlatList.
    Passes key and value to Photo.tsx.

--- Pages/Feed.tsx
+++ Pages/Feed.tsx
@@ -1,13 +1,10 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { FlatList, Text, View } from "react-native";
+import Photo from "../Components/Feed/Photo";
 import { FEED_QUERY } from "../Components/Fragment";
 import ViewContainer from "../Components/ViewContainer";
-import {
-  seeFeed,
-  seeFeedVariables,
-  seeFeed_seeFeed,
-} from "../__generated__/seeFeed";
+import { seeFeed, seeFeedVariables } from "../__generated__/seeFeed";
 
 //성공적인 로그인 => authLink.concat으로 백앤드로 토큰이 보내졌다는 의미이다.
 export default function Feed({ navigation }: any) {
@@ -18,16 +15,15 @@ export default function Feed({ navigation }: any) {
   });
 
   //flatlist는 현재 화면에 보이는것만 랜더링을 한다.
+  //FlatList.showsVerticalScrollIndicator 스크롤바를 지워준다.
   return (
     <ViewContainer loading={loading}>
       <FlatList
+        style={{ width: "100%" }}
         data={data?.seeFeed}
+        showsVerticalScrollIndicator={false}
         keyExtractor={(photo) => "" + photo?.id}
-        renderItem={({ item: photo }) => (
-          <View>
-            <Text>{photo?.caption}</Text>
-          </View>
-        )}
+        renderItem={({ item: photo }) => <Photo key={photo?.id} {...photo!} />}
       />
     </ViewContainer>
   );
