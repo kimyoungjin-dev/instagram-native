8735af6d8d8388030c01d482353d360eac9b3904
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-22 04:36:10 +0900

    Stack navigation to be used for common use

---
+++ Pages/Photo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Photo() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <Text style={{ color: "white" }}>Photo</Text>
+    </View>
+  );
+}
