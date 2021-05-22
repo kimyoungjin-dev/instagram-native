6463026ba8df6971bbbb2f057032416f7d5209b5
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-22 14:18:45 +0900

    onFreshing: start setRefreshing (true) => execute refetch() => setRefreshing (false)

--- Pages/Feed.tsx
+++ Pages/Feed.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { FlatList } from "react-native";
 import Photo from "../Components/Feed/Photo";
 import { FEED_QUERY } from "../Components/Fragment";
@@ -7,19 +7,30 @@ import ViewContainer from "../Components/ViewContainer";
 import { seeFeed, seeFeedVariables } from "../__generated__/seeFeed";
 
 //성공적인 로그인 => authLink.concat으로 백앤드로 토큰이 보내졌다는 의미이다.
-export default function Feed({ navigation }: any) {
-  const { data, loading } = useQuery<seeFeed, seeFeedVariables>(FEED_QUERY, {
-    variables: {
-      page: 1,
-    },
-  });
+export default function Feed() {
+  const [refreshing, setRefreshing] = useState(false);
+  const { data, loading, refetch } = useQuery<seeFeed, seeFeedVariables>(
+    FEED_QUERY,
+    {
+      variables: {
+        page: 1,
+      },
+    }
+  );
+
+  const onFreshing = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
 
   //flatlist는 현재 화면에 보이는것만 랜더링을 한다.
   //FlatList.showsVerticalScrollIndicator 스크롤바를 지워준다.
-
   return (
     <ViewContainer loading={loading}>
       <FlatList
+        refreshing={refreshing}
+        onRefresh={onFreshing}
         style={{ width: "100%" }}
         data={data?.seeFeed}
         showsVerticalScrollIndicator={false}
