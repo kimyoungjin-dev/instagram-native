83636e6e0975c549cd2a06b673a5f1e78c3f22e8
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-22 13:29:32 +0900

    Create a shared Stack navigation.

---
+++ Pages/Likes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Likes() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <Text style={{ color: "white" }}>Likes</Text>
+    </View>
+  );
+}
