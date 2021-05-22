f404a6e960c6605b6811861668a624400445ae5f
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-23 00:56:50 +0900

    Write LIKES_QUERY using useQuery
    fetch useQuery IKES_QUERY variable (id)
    The value of ID is photoId, and photoId is received as params from Photo.tsx.

--- Pages/Likes.tsx
+++ Pages/Likes.tsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
 import { Text, View } from "react-native";
+import { LIKES_QUERY } from "../Components/Fragment";
+import {
+  seePhotoLikes,
+  seePhotoLikesVariables,
+} from "../__generated__/seePhotoLikes";
 
-export default function Likes() {
+export default function Likes({ route }: any) {
+  const { data } = useQuery<seePhotoLikes, seePhotoLikesVariables>(
+    LIKES_QUERY,
+    {
+      variables: {
+        id: route?.params?.photoId,
+      },
+    }
+  );
+  console.log(data);
   return (
     <View
       style={{
