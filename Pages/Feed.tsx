4164b5669bae3ec978c6f1728675d388a9afc517
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-22 14:42:16 +0900

    Update Feed.tsx
    FlatList. Add RefreshControl
    RefreshControl: Put tintColor & onRefresh & refreshing properties.

--- Pages/Feed.tsx
+++ Pages/Feed.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
 import React, { useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 import Photo from "../Components/Feed/Photo";
 import { FEED_QUERY } from "../Components/Fragment";
 import ViewContainer from "../Components/ViewContainer";
@@ -8,7 +8,6 @@ import { seeFeed, seeFeedVariables } from "../__generated__/seeFeed";
 
 //성공적인 로그인 => authLink.concat으로 백앤드로 토큰이 보내졌다는 의미이다.
 export default function Feed() {
-  const [refreshing, setRefreshing] = useState(false);
   const { data, loading, refetch } = useQuery<seeFeed, seeFeedVariables>(
     FEED_QUERY,
     {
@@ -18,19 +17,26 @@ export default function Feed() {
     }
   );
 
-  const onFreshing = async () => {
+  const refresh = async () => {
     setRefreshing(true);
     await refetch();
     setRefreshing(false);
   };
+
+  const [refreshing, setRefreshing] = useState(false);
 
   //flatlist는 현재 화면에 보이는것만 랜더링을 한다.
   //FlatList.showsVerticalScrollIndicator 스크롤바를 지워준다.
   return (
     <ViewContainer loading={loading}>
       <FlatList
-        refreshing={refreshing}
-        onRefresh={onFreshing}
+        refreshControl={
+          <RefreshControl
+            tintColor="white"
+            refreshing={refreshing}
+            onRefresh={refresh}
+          />
+        }
         style={{ width: "100%" }}
         data={data?.seeFeed}
         showsVerticalScrollIndicator={false}
