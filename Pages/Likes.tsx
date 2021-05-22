7184abafe9ab37a9db04461d49f50ce72e854ec6
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-23 02:09:27 +0900

    Update Likes.tsx
    Create refresh with RefreshControl
    
    Likes component: Shows the list of people who clicked like.

--- Pages/Likes.tsx
+++ Pages/Likes.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@apollo/client";
-import React from "react";
-import { FlatList, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, RefreshControl, Text, View } from "react-native";
 import { LIKES_QUERY } from "../Components/Fragment";
+import UserRow from "../Components/UserRow";
 import ViewContainer from "../Components/ViewContainer";
 import {
   seePhotoLikes,
@@ -10,29 +11,42 @@ import {
 
 //photo 아이디가 존재하지않는다면 skip! useQuery를 건너뛴다.
 export default function Likes({ route }: any) {
-  const { data, loading } = useQuery<seePhotoLikes, seePhotoLikesVariables>(
-    LIKES_QUERY,
-    {
-      variables: {
-        id: route?.params?.photoId,
-      },
-      skip: !route?.params?.photoId,
-    }
-  );
+  const [refreshing, setRefreshing] = useState(false);
+  const { data, loading, refetch } = useQuery<
+    seePhotoLikes,
+    seePhotoLikesVariables
+  >(LIKES_QUERY, {
+    variables: {
+      id: route?.params?.photoId,
+    },
+    skip: !route?.params?.photoId,
+  });
 
   // keyExtractor : 필수:string
   // data : query 명
+
+  const onRefreshing = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
   return (
     <ViewContainer loading={loading}>
       <FlatList
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefreshing}
+            tintColor="white"
+          />
+        }
+        refreshing={refreshing}
+        onRefresh={onRefreshing}
         style={{ width: "100%" }}
         data={data?.seePhotoLikes}
         keyExtractor={(item) => "" + item?.id}
-        renderItem={({ item: user }) => (
-          <View>
-            <Text>{user?.username}</Text>
-          </View>
-        )}
+        renderItem={({ item: user }) => <UserRow {...user!} />}
       />
     </ViewContainer>
   );
