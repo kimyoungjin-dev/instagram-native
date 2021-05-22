d56cbc650124536c0228e80d4ae05518e3cb82a4
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-23 00:55:27 +0900

    Get the LIKES_QUERY query from the backend.
    Create a UserFragment that contains user information.

---
+++ __generated__/UserFragment.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: UserFragment
+// ====================================================
+
+export interface UserFragment {
+  __typename: "User";
+  username: string;
+  avatar: string | null;
+  isMe: boolean;
+  isFollowing: boolean;
+  id: number;
+}
