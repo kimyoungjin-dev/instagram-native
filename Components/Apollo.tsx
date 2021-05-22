a5f3605f933e47a4a23b319878517bf5b9904a9e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-22 21:23:48 +0900

    Delete Comments

--- Components/Apollo.tsx
+++ Components/Apollo.tsx
@@ -38,22 +38,14 @@ const authLink = setContext((_, { headers }) => {
     },
   };
 });
-//fetchMore을 사용하기위한 조건은 새롭게 추가된 데이터를 cache에 반영하도록 해준다.
-//typePolicies apollo 에게 type을 설정 할 수 있도록 한다.
-//apollo는 쿼리를 독립된 폴더에 저장을 하는데, 기존page , 변경된 page는 독립되어 저장되지 않도록 해준다.
-//keyArgs : seeFeed의 변수를 하나의 폴더에 저장하는 셈이다.
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
         fields: {
-          seeFeed: {
-            keyArgs: false,
-            merge(existing = [], incoming = []) {
-              return [...existing, ...incoming];
-            },
-          },
+          seeFeed: offsetLimitPagination(),
         },
       },
     },
