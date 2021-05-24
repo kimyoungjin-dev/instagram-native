5202bece83af2a9f0a292adde03838c7078856af
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-24 14:58:38 +0900

    Update Comments.tsx
    Receive comments from the photo,
    Pass props as Comment.
    
    With CommentCount, the number of comments is rendered on the screen.

--- Pages/Comments.tsx
+++ Pages/Comments.tsx
@@ -1,17 +1,49 @@
 import React from "react";
 import { Text, View } from "react-native";
+import styled from "styled-components/native";
+import {
+  seeFeed_seeFeed,
+  seeFeed_seeFeed_user,
+} from "../__generated__/seeFeed";
+import Comment from "./Comment";
 
-export default function Comments() {
+interface UpdatedProps
+  extends Pick<seeFeed_seeFeed, "comments" | "commentNumber" | "caption"> {
+  author: seeFeed_seeFeed_user["username"];
+  photoId: seeFeed_seeFeed["id"];
+  userId: seeFeed_seeFeed_user["id"];
+}
+
+const CommentCount = styled.Text`
+  color: orange;
+  font-weight: bold;
+  margin-top: 7px;
+  margin-bottom: 1px;
+`;
+
+export default function Comments({
+  photoId,
+  author,
+  caption,
+  commentNumber,
+  comments,
+  userId,
+}: UpdatedProps) {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "black",
-      }}
-    >
-      <Text style={{ color: "white" }}>Comments</Text>
+    <View>
+      <Comment author={author} payload={caption!} userId={userId} />
+      <CommentCount>
+        {commentNumber === 0 ? "" : `${commentNumber} Comments`}
+      </CommentCount>
+
+      {comments?.map((item) => (
+        <Comment
+          key={item?.id}
+          author={item?.user.username!}
+          payload={item?.payload!}
+          userId={userId}
+        />
+      ))}
     </View>
   );
 }
