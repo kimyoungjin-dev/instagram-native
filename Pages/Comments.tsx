369abfe439e2e4fd1cd3690043156c9771b6a5ec
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-24 16:34:34 +0900

    Create a createCommentMutation && Create a form using useForm().
    manage register payload with useEffect)
    
    Create createCommentMutation.
    
    newComment: New comment to be input
    
    newCacheComment: Use cache.writeFragment to register in the cache.
    
    cache.modify = id: Changes photoId, comments, commentNumber.

--- Pages/Comments.tsx
+++ Pages/Comments.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import React, { useEffect } from "react";
+import { SubmitHandler, useForm, useWatch } from "react-hook-form";
 import { View } from "react-native";
 import styled from "styled-components/native";
+import { CREAT_COMMENT_MUTATION } from "../Components/Fragment";
+import useMe from "../Components/useMe";
+import {
+  createComment,
+  createCommentVariables,
+} from "../__generated__/createComment";
 import {
   seeFeed_seeFeed,
   seeFeed_seeFeed_user,
@@ -21,6 +29,10 @@ const CommentCount = styled.Text`
   margin-bottom: 1px;
 `;
 
+const Input = styled.TextInput`
+  color: white;
+`;
+
 export default function Comments({
   photoId,
   author,
@@ -29,6 +41,86 @@ export default function Comments({
   comments,
   userId,
 }: UpdatedProps) {
+  const { register, handleSubmit, setValue, getValues } = useForm();
+  const { data: userData } = useMe();
+
+  useEffect(() => {
+    register("payload", {
+      required: true,
+    });
+  }, [register]);
+
+  const [createCommentMutation, { loading }] = useMutation<
+    createComment,
+    createCommentVariables
+  >(CREAT_COMMENT_MUTATION, {
+    update: (cache, result) => {
+      const { payload } = getValues();
+      // setvalue("payload", "")
+      if (result.data?.createComment.ok) {
+        const {
+          data: {
+            createComment: { ok, id },
+          },
+        } = result;
+        if (ok && userData?.me) {
+          const newComment = {
+            __typename: "Comment",
+            id,
+            payload,
+            isMine: true,
+            createdAt: Date.now() + "",
+            user: {
+              ...userData?.me,
+            },
+          };
+          const newCacheComment = cache.writeFragment({
+            data: newComment,
+            fragment: gql`
+              fragment BSName on Comment {
+                id
+                user {
+                  username
+                  avatar
+                }
+                payload
+                isMine
+                createdAt
+              }
+            `,
+          });
+
+          cache.modify({
+            id: `Photo:${photoId}`,
+            fields: {
+              comments(prev) {
+                return [...prev, newCacheComment];
+              },
+              commentNumber(prev) {
+                return [prev + 1];
+              },
+            },
+          });
+        }
+      }
+    },
+  });
+
+  const onSubmit: SubmitHandler<createCommentVariables> = (data) => {
+    const { payload } = data;
+
+    if (loading) {
+      return;
+    } else {
+      createCommentMutation({
+        variables: {
+          payload,
+          photoId,
+        },
+      });
+    }
+  };
+
   return (
     <View>
       <Comment author={author} payload={caption!} userId={userId} />
@@ -39,11 +131,26 @@ export default function Comments({
       {comments?.map((item) => (
         <Comment
           key={item?.id}
+          userId={userId}
+          commentId={item?.id}
           author={item?.user.username!}
           payload={item?.payload!}
-          userId={userId}
+          isMine={item?.isMine}
+          photoId={photoId}
         />
       ))}
+
+      <View>
+        <Input
+          onSubmitEditing={handleSubmit(onSubmit)}
+          placeholder="Write Your Comments..."
+          placeholderTextColor="white"
+          autoCapitalize="none"
+          onChangeText={(text) => {
+            setValue("payload", text);
+          }}
+        />
+      </View>
     </View>
   );
 }
