8b1e9392908371dfd8c63fcc91b6c2ae0e1ea268
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-24 14:59:11 +0900

    Create Comment.tsx
    Receives props from the comments component, and shows the name and message of the user who wrote the comment.

---
+++ Pages/Comment.tsx
@@ -0,0 +1,45 @@
+import { parseType } from "graphql";
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import FindHahtag from "../Components/FindHahtag";
+import {
+  seeFeed_seeFeed_comments,
+  seeFeed_seeFeed_comments_user,
+  seeFeed_seeFeed_user,
+} from "../__generated__/seeFeed";
+import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/core";
+
+interface UpdatedProps {
+  payload: seeFeed_seeFeed_comments["payload"];
+  author: seeFeed_seeFeed_comments_user["username"];
+  userId: seeFeed_seeFeed_user["id"];
+}
+
+const Container = styled.View`
+  flex-direction: row;
+`;
+
+const CommentUser = styled.Text`
+  color: white;
+  font-weight: bold;
+`;
+
+export default function Comment({ author, payload, userId }: UpdatedProps) {
+  const navigation = useNavigation();
+  const goToProfile = () => {
+    navigation.navigate("Profile", {
+      author,
+      userId,
+    });
+  };
+
+  return (
+    <Container>
+      <TouchableOpacity onPress={goToProfile}>
+        <CommentUser>{author}</CommentUser>
+      </TouchableOpacity>
+      <FindHahtag caption={payload} />
+    </Container>
+  );
+}
