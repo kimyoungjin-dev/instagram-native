1bc4562246c35ca001f0e3092a8d77d98c333628
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-24 15:42:39 +0900

    deleteCommentMutation ,cache Update
    Get deleteCommentMutation and update the cache. (Implementation of evict deletion function),
    
    After deletion: Give commentNumber -1.

--- Pages/Comment.tsx
+++ Pages/Comment.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import FindHahtag from "../Components/FindHahtag";
 import {
+  seeFeed_seeFeed,
   seeFeed_seeFeed_comments,
   seeFeed_seeFeed_comments_user,
   seeFeed_seeFeed_user,
 } from "../__generated__/seeFeed";
 import styled from "styled-components/native";
 import { useNavigation } from "@react-navigation/core";
+import { useMutation } from "@apollo/client";
+import { DELETE_COMMENT_MUTATION } from "../Components/Fragment";
+import {
+  deleteComment,
+  deleteCommentVariables,
+} from "../__generated__/deleteComment";
 
 const Container = styled.View`
   flex-direction: row;
+  align-items: center;
 `;
 
 const CommentUser = styled.Text`
@@ -18,13 +26,60 @@ const CommentUser = styled.Text`
   font-weight: bold;
 `;
 
+const DelteCommentText = styled.Text`
+  color: tomato;
+  font-size: 12px;
+`;
+
 interface UpdatedProps {
+  photoId?: seeFeed_seeFeed["id"];
   payload: seeFeed_seeFeed_comments["payload"];
+  isMine?: seeFeed_seeFeed_comments["isMine"];
   author: seeFeed_seeFeed_comments_user["username"];
-  userId: seeFeed_seeFeed_user["id"];
+  commentId?: seeFeed_seeFeed_comments["id"];
+  userId?: seeFeed_seeFeed_user["id"];
 }
 
-export default function Comment({ author, payload, userId }: UpdatedProps) {
+export default function Comment({
+  author,
+  payload,
+  userId,
+  commentId,
+  isMine,
+  photoId,
+}: UpdatedProps) {
+  const [deleteCommentMutation] = useMutation<
+    deleteComment,
+    deleteCommentVariables
+  >(DELETE_COMMENT_MUTATION, {
+    variables: {
+      id: commentId!,
+    },
+    update: (cache, result) => {
+      if (result.data?.deleteComment.ok) {
+        const {
+          data: {
+            deleteComment: { ok },
+          },
+        } = result;
+        if (ok) {
+          cache.evict({
+            id: `Comment:${commentId}`,
+          });
+
+          cache.modify({
+            id: `Photo:${photoId}`,
+            fields: {
+              commentNumber(prev) {
+                return prev - 1;
+              },
+            },
+          });
+        }
+      }
+    },
+  });
+
   const navigation = useNavigation();
   const goToProfile = () => {
     navigation.navigate("Profile", {
@@ -33,12 +88,21 @@ export default function Comment({ author, payload, userId }: UpdatedProps) {
     });
   };
 
+  const onDelete = () => {
+    deleteCommentMutation();
+  };
+
   return (
     <Container>
       <TouchableOpacity onPress={goToProfile}>
         <CommentUser>{author}</CommentUser>
       </TouchableOpacity>
       <FindHahtag caption={payload} />
+      {isMine ? (
+        <TouchableOpacity onPress={() => onDelete()}>
+          <DelteCommentText>삭제</DelteCommentText>
+        </TouchableOpacity>
+      ) : null}
     </Container>
   );
 }
