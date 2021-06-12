70372057119041996620ff31f263544ab58b4b7e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-12 18:13:37 +0900

    Create a modeColor function and make it available to all components.
    +css수정 및 코드정리

--- Components/Pages/Welcome.tsx
+++ Components/Pages/Welcome.tsx
@@ -2,9 +2,10 @@ import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
 import { RootStackParamList } from "../RootStack";
 import styled from "styled-components/native";
-import { defaultBox } from "../Shared/SharedStyles";
-import { Text, TouchableOpacity, View } from "react-native";
+import { defaultBox, modeColor } from "../Shared/SharedStyles";
+import { TouchableOpacity } from "react-native";
 import DarkModeSwitch from "../Shared/DarkModeSwitch";
+import { useTheme } from "../styles/ChangeMode";
 
 type Props = StackScreenProps<RootStackParamList, "Welcome">;
 
@@ -39,13 +40,14 @@ const LoginText = styled.Text`
   text-align: center;
 `;
 
-const SignUpMessage = styled.View`
+const SignUp = styled.View`
   flex-direction: row;
 `;
 
 const SignUpText = styled.Text`
   color: ${(props) => props.theme.bgColor};
   margin-left: 8px;
+  font-size: 16px;
 `;
 
 const FirstVisit = styled.Text`
@@ -58,22 +60,25 @@ export default function Welcome({ navigation }: Props) {
       <DarkModeBox>
         <DarkModeSwitch />
       </DarkModeBox>
+
       <Logo
         resizeMode="center"
         source={require("../../assets/instagram_logo.png")}
+        style={{ tintColor: modeColor() }}
       />
+
       <Login>
         <TouchableOpacity onPress={() => navigation.navigate("Login")}>
           <LoginText>로그인</LoginText>
         </TouchableOpacity>
       </Login>
 
-      <SignUpMessage>
+      <SignUp>
         <FirstVisit>instagram에 처음 오셨나요?</FirstVisit>
         <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
           <SignUpText>가입하기</SignUpText>
         </TouchableOpacity>
-      </SignUpMessage>
+      </SignUp>
     </Container>
   );
 }
