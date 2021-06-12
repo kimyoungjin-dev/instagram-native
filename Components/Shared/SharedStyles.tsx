83371f5678350c8f15afe815341301e9757e8231
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-12 18:13:47 +0900

    Create a modeColor function and make it available to all components

--- Components/Shared/SharedStyles.tsx
+++ Components/Shared/SharedStyles.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components/native";
+import { useTheme } from "../styles/ChangeMode";
 
 export const defaultBox = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
+
+export const modeColor = () => {
+  const { mode } = useTheme();
+  return mode === "light" ? "white" : "black";
+};
