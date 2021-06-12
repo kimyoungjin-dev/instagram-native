1ec1b777b7bea768faae4f235c5ac582eb67227d
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-12 17:27:07 +0900

    Change light , dark theme color

--- Components/styles/styles.ts
+++ Components/styles/styles.ts
@@ -1,16 +1,16 @@
 import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
-  bgColor: "#FAFAFA",
-  fontColor: "rgb(38,38,38)",
+  bgColor: "white",
+  fontColor: "black",
   loginBtnColor: "rgb(69,166,246)",
   borderColor: "rgb(219,219,219)",
   silverColor: "silver",
 };
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "rgb(38,38,38)",
-  fontColor: "#FAFAFA",
+  bgColor: "black",
+  fontColor: "white",
   loginBtnColor: "rgb(69,166,246)",
   borderColor: "rgb(219,219,219)",
   silverColor: "silver",
