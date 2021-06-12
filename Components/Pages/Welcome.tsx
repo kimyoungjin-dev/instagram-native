c6bf9f299b2d6528ae240feeab7cbf7a964ad408
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-12 12:42:23 +0900

    Apply the dark mode switch.

--- Components/Pages/Welcome.tsx
+++ Components/Pages/Welcome.tsx
@@ -1,16 +1,17 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
 import { RootStackParamList } from "../RootStack";
+import styled from "styled-components/native";
+import DarkModeSwitch from "../Shared/DarkModeSwitch";
+
+const Container = styled.View``;
 
 type Props = StackScreenProps<RootStackParamList, "Welcome">;
 
 export default function Welcome({ navigation }: Props) {
   return (
-    <View>
-      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-        <Text>Login 으로 이동</Text>
-      </TouchableOpacity>
-    </View>
+    <Container>
+      <DarkModeSwitch />
+    </Container>
   );
 }
