6b297c568c760fe301347a88f6f2d06cf1d4baa6
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-12 10:39:41 +0900

    Create a startAsync function (download images and fonts before loading)

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import AppLoading from "expo-app-loading";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import Font from "expo-font";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const onFinish = () => setIsLoading(true);
-  const preLoad = async () => {};
+  const preLoad = async () => {
+    //font
+    const fontsToLoad = [Ionicons.font, FontAwesome.font];
+    const fontPromise = fontsToLoad.map((font) => Font.loadAsync(font));
+    //image
+    const imageToLoad = [require("./assets/instagram_logo.png")];
+    const imagePromise = imageToLoad.map((image) => Asset.loadAsync(image));
+    //promiss
+    await Promise.all([fontPromise, imagePromise]);
+  };
 
   return (
     <>
