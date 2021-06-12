7f21962afeaca1e12f297edff1072988609d7dc1
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-12 18:36:01 +0900

    Code Cleaning

--- Components/Shared/SharedStyles.tsx
+++ Components/Shared/SharedStyles.tsx
@@ -1,6 +1,4 @@
-import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
-import { useTheme } from "../styles/ChangeMode";
 
 export const defaultBox = styled.View`
   flex: 1;
