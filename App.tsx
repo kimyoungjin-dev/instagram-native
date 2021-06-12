6e02c962bf0f7c311d3109c3f761e7031bd14aff
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-12 12:41:16 +0900

    Apply dark mode to subcomponents of ThemeManager.
    Wrap the navigation container with AppearanceProvider & ThemeProvider & ThemeProvider.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import { View, Text, Image } from "react-native";
+
+import { Appearance, AppearanceProvider } from "react-native-appearance";
 import AppLoading from "expo-app-loading";
 import { Ionicons, FontAwesome } from "@expo/vector-icons";
 import Font from "expo-font";
 import { Asset } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
 import LogOutNav from "./Navigation/LogOutNav";
+import { ThemeProvider } from "styled-components/native";
+import { darkTheme, lightTheme } from "./Components/styles/styles";
+import { useTheme } from "./Components/styles/ChangeMode";
+import ThemeManager from "./Components/styles/ThemeManaget";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -21,6 +26,9 @@ export default function App() {
     await Promise.all([fontPromise, imagePromise]);
   };
 
+  const light = Appearance.getColorScheme() === "light";
+  const { mode } = useTheme();
+  const darkMode = mode === "light" ? lightTheme : darkTheme;
   return (
     <>
       {isLoading ? (
@@ -30,9 +38,15 @@ export default function App() {
           onFinish={onFinish}
         />
       ) : (
-        <NavigationContainer>
-          <LogOutNav />
-        </NavigationContainer>
+        <AppearanceProvider>
+          <ThemeProvider theme={light ? lightTheme : darkTheme}>
+            <ThemeManager>
+              <NavigationContainer>
+                <LogOutNav />
+              </NavigationContainer>
+            </ThemeManager>
+          </ThemeProvider>
+        </AppearanceProvider>
       )}
     </>
   );
