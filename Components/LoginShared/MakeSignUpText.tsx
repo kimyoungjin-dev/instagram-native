146a38b3769451b0ec7afdeca9036ed64fd3f7b8
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-12 19:28:15 +0900

    Link to SignUp.tsx to sign up

---
+++ Components/LoginShared/MakeSignUpText.tsx
@@ -0,0 +1,36 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { MakeSignUpProps } from "../Shared/InterFace";
+
+const SignUp = styled.View`
+  flex-direction: row;
+`;
+
+const SignUpText = styled.Text`
+  color: ${(props) => props.theme.bgColor};
+  margin-left: 8px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const FirstVisit = styled.Text`
+  color: ${(props) => props.theme.bgColor};
+`;
+
+export default function MakeSignUpText({
+  text,
+  colorText,
+  link,
+}: MakeSignUpProps) {
+  const navigation = useNavigation();
+  return (
+    <SignUp>
+      <FirstVisit>{text}</FirstVisit>
+      <TouchableOpacity onPress={() => navigation.navigate(`${link}`)}>
+        <SignUpText>{colorText}</SignUpText>
+      </TouchableOpacity>
+    </SignUp>
+  );
+}
--- Components/Pages/Welcome.tsx
+++ Components/Pages/Welcome.tsx
@@ -1,49 +1,27 @@
-import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
-import { RootStackParamList } from "../RootStack";
 import styled from "styled-components/native";
 import { defaultBox } from "../Shared/SharedStyles";
-import { TouchableOpacity } from "react-native";
 import DarkModeSwitch from "../Shared/DarkModeSwitch";
 import SubmitBtn from "../LoginShared/SubmitBtn";
 import Logo from "../LoginShared/Logo";
-import { WelcomeProps } from "../../Navigation/NavigationProps";
+import MakeSignUpText from "../LoginShared/MakeSignUpText";
 
 const Container = styled(defaultBox)`
   background-color: ${(props) => props.theme.fontColor};
   padding: 0px 20px;
 `;
 
-const SignUp = styled.View`
-  flex-direction: row;
-`;
-
-const SignUpText = styled.Text`
-  color: ${(props) => props.theme.bgColor};
-  margin-left: 8px;
-  font-size: 16px;
-`;
-
-const FirstVisit = styled.Text`
-  color: ${(props) => props.theme.bgColor};
-`;
-
-export default function Welcome({ navigation }: WelcomeProps) {
+export default function Welcome() {
   return (
     <Container>
       <DarkModeSwitch />
-
       <Logo />
-
       <SubmitBtn text="로그인" routerName="Login" />
-
-      <SignUp>
-        <FirstVisit>instagram에 처음 오셨나요?</FirstVisit>
-
-        <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
-          <SignUpText>가입하기</SignUpText>
-        </TouchableOpacity>
-      </SignUp>
+      <MakeSignUpText
+        text="instagram에 처음오셨나요?"
+        colorText="가입하기"
+        link="SignUp"
+      />
     </Container>
   );
 }
