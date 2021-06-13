c39c1616c531174f9048b856008ac90d71832971
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-13 10:13:23 +0900

    Create a membership registration page. Recycle Login.tsx code

--- Components/Pages/SignUp.tsx
+++ Components/Pages/SignUp.tsx
@@ -1,13 +1,73 @@
 import React from "react";
-import { TouchableOpacity, Text, View } from "react-native";
-import { SignUpProps } from "../../Navigation/NavigationProps";
+import styled from "styled-components/native";
+import { defaultBox } from "../Shared/SharedStyles";
+import Logo from "../LoginShared/Logo";
+import DarkModeSwitch from "../Shared/DarkModeSwitch";
+import { TextInput } from "../LoginShared/TextInput";
+import Form from "../LoginShared/Form";
+import SubmitBtn from "../LoginShared/SubmitBtn";
+import { modeColor } from "../Shared/SharedFunction";
+import MakeSignUpText from "../LoginShared/MakeSignUpText";
 
-export default function SignUp({ navigation }: SignUpProps) {
+const Container = styled(defaultBox)`
+  background-color: ${(props) => props.theme.fontColor};
+  padding: 0px 20px;
+`;
+
+const SignUpText = styled.Text`
+  font-weight: bold;
+  font-size: 17px;
+  margin-bottom: 20px;
+`;
+
+export default function SignUp() {
   return (
-    <View>
-      <TouchableOpacity onPress={() => navigation.navigate("Welcome")}>
-        <Text>Welcome Page 이동</Text>
-      </TouchableOpacity>
-    </View>
+    <Container>
+      <DarkModeSwitch />
+      <Logo />
+      <SignUpText>인스타그램 계정 만들기</SignUpText>
+      <Form>
+        <TextInput
+          placeholder="User Name"
+          placeholderTextColor={modeColor()}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="First Name"
+          placeholderTextColor={modeColor()}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="Last Name"
+          placeholderTextColor={modeColor()}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="Email"
+          placeholderTextColor={modeColor()}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor={modeColor()}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+        />
+
+        <SubmitBtn text="가입" />
+      </Form>
+
+      <MakeSignUpText
+        link="Login"
+        text="계정이 있으신가요?"
+        colorText="로그인"
+      />
+    </Container>
   );
 }
