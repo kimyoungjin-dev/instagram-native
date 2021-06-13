afc15ca738214ff3eefeda5f2784cabe5d925419
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-13 14:31:09 +0900

    Code Cleanup

--- Components/Pages/Login.tsx
+++ Components/Pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import Logo from "../LoginShared/Logo";
 import SubmitBtn from "../LoginShared/SubmitBtn";
 import Form from "../LoginShared/Form";
--- Components/Pages/SignUp.tsx
+++ Components/Pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import styled from "styled-components/native";
 import Logo from "../LoginShared/Logo";
 import Form from "../LoginShared/Form";
