04a0db1605c233befede080094830a6ecd7ed4e7
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-13 10:36:48 +0900

    Create reverseModeColor as opposed to modeColor.

--- Components/Shared/SharedFunction.tsx
+++ Components/Shared/SharedFunction.tsx
@@ -4,3 +4,8 @@ export const modeColor = () => {
   const { mode } = useTheme();
   return mode === "light" ? "black" : "white";
 };
+
+export const reverseModeColor = () => {
+  const { mode } = useTheme();
+  return mode === "light" ? "white" : "black";
+};
