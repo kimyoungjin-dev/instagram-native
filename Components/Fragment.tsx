6e777481da907908744d0b90612d946cdd68bbe8
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-14 08:59:52 +0900

    Create LOGIN_MUTATION,CREATE_ACCOUNT_MUTATION

---
+++ Components/Fragment.tsx
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      ok
+      error
+      token
+    }
+  }
+`;
+
+export const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
