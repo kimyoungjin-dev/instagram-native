a69b8b541b2dd5e249548a831b7d0876811e163a
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-13 15:32:57 +0900

    Change props name

--- Components/Pages/Welcome.tsx
+++ Components/Pages/Welcome.tsx
@@ -5,18 +5,19 @@ import DarkModeSwitch from "../Shared/DarkModeSwitch";
 import SubmitBtn from "../LoginShared/SubmitBtn";
 import Logo from "../LoginShared/Logo";
 import MakeSignUpText from "../LoginShared/MakeSignUpText";
+import { WelcomeNavProps } from "../../Navigation/NavigationProps";
 
 const Container = styled(defaultBox)`
   background-color: ${(props) => props.theme.fontColor};
   padding: 0px 20px;
 `;
 
-export default function Welcome() {
+export default function Welcome({ navigation }: WelcomeNavProps) {
   return (
     <Container>
       <DarkModeSwitch />
       <Logo />
-      <SubmitBtn text="로그인" routerName="Login" />
+      <SubmitBtn text="로그인" onPress={() => navigation.navigate("Login")} />
       <MakeSignUpText
         text="instagram에 처음오셨나요?"
         colorText="가입하기"
--- Navigation/NavigationProps.tsx
+++ Navigation/NavigationProps.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import { RootStackParamList } from "../Components/RootStack";
 
-export type WelcomeProps = StackScreenProps<RootStackParamList, "Welcome">;
-export type LoginProps = StackScreenProps<RootStackParamList, "Login">;
-export type SignUpProps = StackScreenProps<RootStackParamList, "SignUp">;
+export type WelcomeNavProps = StackScreenProps<RootStackParamList, "Welcome">;
+export type LoginNavProps = StackScreenProps<RootStackParamList, "Login">;
+export type SignUpNavProps = StackScreenProps<RootStackParamList, "SignUp">;
