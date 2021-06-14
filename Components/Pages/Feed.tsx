fa9a23cdb46a601166b5f76f1bc3aba2d9642dec
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-14 13:42:20 +0900

    Write FEED_QUERY

--- Components/Pages/Feed.tsx
+++ Components/Pages/Feed.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
 import { defaultBox } from "../Shared/SharedStyles";
-import { Text } from "react-native";
-import { FeedNavProps } from "../../Navigation/NavigationProps";
+import { Text, TouchableOpacity } from "react-native";
+import { useQuery } from "@apollo/client";
+import { FEED_QUERY } from "../Fragment";
+import { seeFeed, seeFeedVariables } from "../../__generated__/seeFeed";
+import { logUserOut } from "../../Apollo";
 
 const Container = styled(defaultBox)`
   background-color: ${(props) => props.theme.fontColor};
 `;
 
-export default function Feed({ navigation }: FeedNavProps) {
+export default function Feed() {
+  const { data } = useQuery<seeFeed, seeFeedVariables>(FEED_QUERY, {
+    variables: {
+      offset: 1,
+    },
+  });
+  console.log(data);
+
   return (
     <Container>
       <Text>Feed</Text>
+      <TouchableOpacity onPress={() => logUserOut()}>
+        <Text>로그아웃</Text>
+      </TouchableOpacity>
     </Container>
   );
 }
