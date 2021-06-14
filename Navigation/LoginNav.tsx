5c8cf267eb4dd2b490bdae79f07bfdc7e486d0c0
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-14 11:13:03 +0900

    Add tap icon. (Apply dark mode)

--- Navigation/LoginNav.tsx
+++ Navigation/LoginNav.tsx
@@ -2,13 +2,83 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { RootStackParamList } from "../Components/RootStack";
 import Feed from "../Components/Pages/Feed";
+import Search from "../Components/Pages/Search";
+import Notifications from "../Components/Pages/Notifications";
+import Profile from "../Components/Pages/Profile";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  modeColor,
+  reverseModeColor,
+} from "../Components/Shared/SharedFunction";
 
 const Tabs = createBottomTabNavigator<RootStackParamList>();
 
 export default function LogInNav() {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Feed" component={Feed} />
+    <Tabs.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: reverseModeColor(),
+          borderTopColor: reverseModeColor(),
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={24}
+              color={modeColor()}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "search" : "search-outline"}
+              size={24}
+              color={modeColor()}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "heart" : "heart-outline"}
+              size={24}
+              color={modeColor()}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={24}
+              color={modeColor()}
+            />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 }
