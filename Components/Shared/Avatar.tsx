eaace5f241e96c2c07c83dd92164b5d35b6255d2
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-14 20:11:57 +0900

    Create an avatar component
    Create user profile avatar

--- Components/Shared/Avatar.tsx
+++ Components/Shared/Avatar.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components/native";
+import { AvatarProps } from "./InterFace";
 
-const Image = styled.Image`
-  width: 35px;
-  height: 35px;
+const Image = styled.Image<AvatarProps>`
+  width: 30px;
+  height: 30px;
   border-radius: 100px;
+  margin-right: ${(props) => (props.isMargin ? "5px" : "0px")};
 `;
 
-export default function Avatar({ uri }: { uri: string | undefined | null }) {
-  return !uri || uri === "" ? null : <Image source={{ uri }} />;
+export default function Avatar({ uri, isMargin }: AvatarProps) {
+  return !uri || uri === "" ? null : (
+    <Image source={{ uri }} isMargin={isMargin} />
+  );
 }
