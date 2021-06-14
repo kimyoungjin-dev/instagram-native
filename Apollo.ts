99bd619f50a58b8d5807bc567cb6b21af0d88755
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-14 13:44:20 +0900

    Check if it is the same as the token of the backend using setContext.
    logout: make token string an empty array
    
    Login: give token string to token

--- Apollo.ts
+++ Apollo.ts
@@ -1,23 +1,45 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { setContext } from "@apollo/client/link/context";
 
 export const TOKEN = "TOKEN";
 
-export const isLoggedInVar = makeVar(Boolean(AsyncStorage.getItem(TOKEN)));
+export const isLoggedInVar = makeVar(false);
+export const tokenVar = makeVar("");
 
 export const logUserIn = async (token: string | null) => {
-  await AsyncStorage.setItem(TOKEN, JSON.stringify(token));
+  await AsyncStorage.setItem(TOKEN, token!);
   isLoggedInVar(true);
+  tokenVar(token!);
 };
 
 export const logUserOut = async () => {
-  AsyncStorage.removeItem(TOKEN);
+  await AsyncStorage.removeItem(TOKEN);
   isLoggedInVar(false);
+  tokenVar("");
 };
 
-const client = new ApolloClient({
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: tokenVar(),
+    },
+  };
+});
+
+const httpLinks = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const client = new ApolloClient({
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLinks),
 });
 
 export default client;
