aab0736f0a042e66a87ee07e6d393cf2b977ed06
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-14 10:27:53 +0900

    Create logUserIn and logUserOut functions.
    logUserIn: put token
    logUserOut: clear token

--- Apollo.ts
+++ Apollo.ts
@@ -1,6 +1,19 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const isLoggedInVar = makeVar(false);
+const TOKEN = "TOKEN";
+
+export const isLoggedInVar = makeVar(Boolean(AsyncStorage.getItem(TOKEN)));
+
+export const logUserIn = async (token: string | null) => {
+  await AsyncStorage.setItem(TOKEN, JSON.stringify(token));
+  isLoggedInVar(true);
+};
+
+export const logUserOut = async () => {
+  AsyncStorage.removeItem(TOKEN);
+  isLoggedInVar(false);
+};
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
