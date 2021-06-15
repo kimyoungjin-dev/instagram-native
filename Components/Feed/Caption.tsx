7a8af423d4a2b0d4782662103fc889f1bc37128c
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-15 10:01:21 +0900

    Displays the caption of the photo and extracts the caption's hashtag using SplitText.

--- Components/Feed/Caption.tsx
+++ Components/Feed/Caption.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import { seeFeed_seeFeed } from "../../__generated__/seeFeed";
-import { Text } from "react-native";
-import {
-  ColorText,
-  FatText,
-  flexRow_AlignCenter,
-} from "../Shared/SharedStyles";
+import { FatText, flexRow_AlignCenter } from "../Shared/SharedStyles";
 import styled from "styled-components/native";
+import SplitText from "../Shared/SplitText";
 
-const Container = styled(flexRow_AlignCenter)``;
+const Container = styled(flexRow_AlignCenter)`
+  margin: 14px 0px;
+`;
 
 const UserName = styled(FatText)`
   margin-right: 7px;
-`;
-
-const Payload = styled.Text`
-  color: ${(props) => props.theme.bgColor};
 `;
 
 type PickCaption = Pick<seeFeed_seeFeed, "caption" | "user">;
@@ -24,17 +18,7 @@ export default function Caption({ caption, user }: PickCaption) {
   return (
     <Container>
       <UserName>{user.username}</UserName>
-      <Text>
-        {caption
-          ?.split(" ")
-          .map((word) =>
-            /#[\w]+/.test(word) ? (
-              <ColorText>{word} </ColorText>
-            ) : (
-              <Payload>{word} </Payload>
-            )
-          )}
-      </Text>
+      <SplitText text={caption} />
     </Container>
   );
 }
