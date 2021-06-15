eccde98b7c59b390f8e0ce4fd0323abb5da8d3fb
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-15 11:59:29 +0900

    Apply Navigation to Hash Tech Words (Hash Tech Navigation)

--- Components/Shared/SplitText.tsx
+++ Components/Shared/SplitText.tsx
@@ -2,23 +2,34 @@ import React from "react";
 import { SplitTextProps } from "./InterFace";
 import { ColorText } from "../Shared/SharedStyles";
 import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
 
 const Text = styled.Text`
   color: ${(props) => props.theme.bgColor};
 `;
 
 export default function SplitText({ text }: SplitTextProps) {
+  const { navigate } = useNavigation();
+
   return !text || text === "" ? null : (
     <Text>
-      {text
-        ?.split(" ")
-        .map((word, index) =>
-          /#[\w]+/.test(word) ? (
-            <ColorText key={index}>{word} </ColorText>
-          ) : (
-            <Text key={index}>{word} </Text>
-          )
-        )}
+      {text?.split(" ").map((word, index) =>
+        /#[\w]+/.test(word) ? (
+          <ColorText
+            key={index}
+            onPress={() =>
+              navigate("Hashtag", {
+                hashtag: word,
+              })
+            }
+          >
+            {word}{" "}
+          </ColorText>
+        ) : (
+          <Text key={index}>{word} </Text>
+        )
+      )}
     </Text>
   );
 }
